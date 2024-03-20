bd13f6877816ec8183e7db7eee2ce45169dbfae9
ברלין תמר < @C19>
2024-03-20 16:04:41 +0200

    branch

---
+++ ConsoleApp1/ConsoleApp1/Branch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.State;
+
+namespace ConsoleApp1
+{
+    internal class Branch
+    {
+        private BranchState _state = null;
+        public Branch() 
+        { 
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/State/BranchState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.State
+{
+    abstract class BranchState
+    {
+        protected Branch _branch;
+     
+
+    }
+}
